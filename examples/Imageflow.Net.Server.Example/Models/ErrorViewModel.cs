3dc4f789b0ac39a51e939cad86121651772380d5
Lilith River <lilith@imazen.io>
2020-04-20 14:01:12 -0600

    Add example web site using the middleware

---
+++ examples/Imageflow.Net.Server.Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Imageflow.Net.Server.Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
