71630393bd99e61de3f88fb12cda0ba552fb52d8
Will Gunn <williamgun@gmail.com>
2020-04-21 11:54:59 -0400

    Basic code cleanup and fixing the bug in IsImagePath so it checks the path for the suffix, and not the suffix for the suffix.

---
+++ src/Imageflow.Net.Server/Structs/ResizeParams.cs
@@ -0,0 +1,13 @@
+﻿namespace Imageflow.Server.Structs
+{
+    public struct ResizeParams
+    {
+        public bool hasParams;
+        public string commandString;
+
+        public override string ToString()
+        {
+            return commandString;
+        }
+    }
+}
