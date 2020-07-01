996ba2ee4110c7ec4e6d43ae27442227e3f9d652
Lilith River <lilith@imazen.io>
2020-07-01 15:46:19 -0600

    Upgrade to Imageflow 1.4.6 and demo ignore_icc_errors command

--- examples/Imageflow.Server.Example/Startup.cs
+++ examples/Imageflow.Server.Example/Startup.cs
@@ -103,6 +103,7 @@ namespace Imageflow.Server.Example
                 .AddCommandDefault("down.filter", "mitchell")
                 .AddCommandDefault("f.sharpen", "15")
                 .AddCommandDefault("webp.quality", "90")
+                .AddCommandDefault("ignore_icc_errors", "true")
                 //When set to true, this only allows ?preset=value URLs, returning 403 if you try to use any other commands. 
                 .SetUsePresetsExclusively(false)
                 .AddPreset(new PresetOptions("large", PresetPriority.DefaultValues)
