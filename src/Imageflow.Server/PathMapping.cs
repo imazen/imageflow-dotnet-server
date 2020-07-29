d77d75bb98b4307cafe87dc3e5ddc6aaebd05103
gerneio <hector.bas.iii@basdeals.com>
2020-07-29 10:59:10 -0500

    Support configurable "IgnoreCase" for MapPath

--- src/Imageflow.Server/PathMapping.cs
+++ src/Imageflow.Server/PathMapping.cs
@@ -2,12 +2,15 @@ namespace Imageflow.Server
 {
     public struct PathMapping
     {
-        public PathMapping(string virtualPath, string physicalPath)
+        public PathMapping(string virtualPath, string physicalPath, bool ignoreCase = false)
         {
             VirtualPath = virtualPath;
             PhysicalPath = physicalPath.TrimEnd('/','\\');
+            IgnoreCase = ignoreCase;
         }
+
         public string VirtualPath { get; }
         public string PhysicalPath { get; }
+        public bool IgnoreCase { get; }
     }
 }
