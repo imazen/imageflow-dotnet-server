bd687b5b97430df526f62ad3702043023f794507
Lilith River <lilith@imazen.io>
2020-09-08 19:01:53 -0600

    Add Imageflow.Server.ExampleDocker

---
+++ examples/Imageflow.Server.ExampleDocker/Startup.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Imageflow.Server.ExampleMinimal
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseImageflow(new ImageflowMiddlewareOptions()
+                .SetMapWebRoot(true));
+            
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    context.Response.ContentType = "text/html";
+                    await context.Response.WriteAsync("<img src=\"fire-umbrella-small.jpg?width=450\" />");
+                });
+            });
+        }
+    }
+}
